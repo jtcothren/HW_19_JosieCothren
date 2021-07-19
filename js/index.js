85cddc4932d58aa70fea92f4c89fdcebcdaad28f
jtcothren <78068370+jtcothren@users.noreply.github.com>
2021-07-18 23:13:52 -0500

    added interaction and scale hover
    added scale hover states to the clickable skills icons
    
    added toggling list menu to skills icons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 console.log("Your idex.js file is loaded correctly!");
+$("#toggle").on("click", function(){
+    $("#dropDown").slideToggle();
+  });
+
+  $("#UItoggle").on("click", function(){
+    $("#UIdropDown").slideToggle();
+  });
+
+  $("#Tooltoggle").on("click", function(){
+    $("#TooldropDown").slideToggle();
+  });
